00b6b287b392b296aa204a44df74dba0b58b9eec
Gabriele <ga2arch@MacBook-Pro-di-Gabriele.local>
2014-02-02 22:35:57 +0100

    forgot s

--- message.py
+++ message.py
@@ -35,7 +35,7 @@ class Message():
     def __str__(self):
         nick = self.get_nick().decode('utf-8', 'ignore')
         s = "%s: %s in %s said to %s: %s" % (self.time, nick, self.chan, self.target, self.msg)
-        return ().encode("utf-8", 'ignore')
+        return (s).encode("utf-8", 'ignore')
     def serialize(self):
         return " @@@ ".join([str(self.time), self.kind, self.nick_full, self.chan, self.msg])
     def deserialize(self, string):
